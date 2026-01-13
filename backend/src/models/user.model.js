1959a6700e0c75fa692465f3ee24019f70f223c4
Deepak Kumar Yadav <dky422003@gmail.comm>
2026-01-14 01:28:36 +0530

    feat: created user schema

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,106 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+
+    password: {
+      type: String,
+      required: true,
+      select: false 
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+
+    isUserAdmin: {
+      type: Boolean,
+      default: false
+    },
+
+    niche: {
+      type: String,
+      default: "Other",
+      enum: [
+        "Art & Creativity",
+        "Technology",
+        "Gaming",
+        "Education",
+        "Business & Finance",
+        "Health & Fitness",
+        "Lifestyle",
+        "Fashion & Beauty",
+        "Food & Cooking",
+        "Travel",
+        "Self Improvement",
+        "Entertainment",
+        "Music",
+        "Photography & Videography",
+        "Podcasting",
+        "News & Commentary",
+        "DIY & Crafts",
+        "Sports",
+        "Science",
+        "Pets & Animals",
+        "Nature & Environment",
+        "Spirituality",
+        "Parenting & Family",
+        "Vlogs",
+        "Automotive",
+        "Real Estate",
+        "Politics",
+        "Non Profit & Social Impact",
+        "Other"
+      ]
+    },
+
+    platforms: {
+      type: [String], 
+      default: ["Other"],
+      enum: [
+        "Instagram",
+        "YouTube",
+        "TikTok",
+        "Facebook",
+        "X",
+        "LinkedIn",
+        "Twitch",
+        "Snapchat",
+        "Other"
+      ]
+    },
+
+    numberOfFollowers: {
+      type: Number,
+      default: 0,
+      min: 0
+    },
+
+    avgLikes: {
+      type: Number,
+      default: 0,
+      min: 0
+    },
+
+    avgComments: {
+      type: Number,
+      default: 0,
+      min: 0
+    }
+  },
+  { timestamps: true }
+);
+
+
+const User = mongoose.model("User", userSchema);
+export default User;
